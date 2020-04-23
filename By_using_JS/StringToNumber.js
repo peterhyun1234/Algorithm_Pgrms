b30f5d3586a93c023e93f752977dec449cebfc18
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-04-23 18:54:01 +0900

    add folder

---
+++ By_using_JS/StringToNumber.js
@@ -0,0 +1,13 @@
+// 내 답
+function solution(s) {
+    var answer = 0;
+    answer = Number(s);
+    return answer;
+}
+
+// 다른 사람 답
+function solution(s) {
+    var answer = 0;
+    answer = parseInt(s, 10);
+    return answer;
+}
